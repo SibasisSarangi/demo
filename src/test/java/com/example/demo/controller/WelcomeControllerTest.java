8d8ddf47105364814d467ca8cdb8057fd4b64c06
Sarangi <sibasis.sarangi@hp.com>
2024-10-10 15:24:55 +0530

    build should fail because of a wrong condition

---
+++ src/test/java/com/example/demo/controller/WelcomeControllerTest.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelcomeControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
